f5422f3f03b9a7ddc96dbce51e3abe9ef44c35f3
Riko Hamblin <r_hamblin@outlook.com>
2016-05-20 20:58:21 -0700

    made modifications to employee class: it now changes its signedIn state automatically
    initially started to implemtent timedisplay.java, validatePin.java

--- app/src/main/java/com/encloode/tick_tock/validate_confirm.java
+++ app/src/main/java/com/encloode/tick_tock/validate_confirm.java
@@ -1,7 +1,9 @@
 package com.encloode.tick_tock;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class validate_confirm extends AppCompatActivity {
 
@@ -10,4 +12,8 @@ public class validate_confirm extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_validate_confirm);
     }
+
+
+
+
 }
