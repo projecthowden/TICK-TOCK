3d0107f996ddd1f29baa92077c072e0907c63614
Ibukun <ioluwayo@ryerson.ca>
2016-05-20 20:49:53 -0400

    implemented the backend java program to handle the add_employee activiy

--- app/src/main/java/com/encloode/tick_tock/add_employee.java
+++ app/src/main/java/com/encloode/tick_tock/add_employee.java
@@ -1,7 +1,10 @@
 package com.encloode.tick_tock;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class add_employee extends AppCompatActivity {
@@ -28,7 +31,34 @@ public class add_employee extends AppCompatActivity {
          */
 
 
+    }
 
+    public void onClick(View v) {
+        EditText editTextPin = (EditText) findViewById(R.id.add_employee_et_pin); // creted an pbject of type edittext view and retrived
+        //and stored the value in a variable after typecasting.
+        int pin = Integer.parseInt(editTextPin.getText().toString());
 
+        EditText editTextName = (EditText) findViewById(R.id.add_employee_et_name);
+        String newEmployeeName = editTextName.getText().toString();
+        //now we need to validate the pin.
+        if (EmployeeDatabase.getNumOfCurrentEmployees() < EmployeeDatabase.maxEmployeeSize) {
+            if (Global.accessDatabase().pinValid(pin)) {
+                //pin is valid...create a new employee and add it to it the employee database.
+                Employee newEmployee = new Employee(newEmployeeName, pin);
+                Global.accessDatabase().addEmployee(newEmployee);// new employee has been added to the database.
+                //research toaast.//go to owner menu activity
+                Intent intent = new Intent(this, ownermenu.class);
+                startActivity(intent);
+                //employee is sucessfully
+                //now we return to the owner menu
+            }
+            else {
+                //notify user that pin  is taken.
+            }
+        }
+        else {
+            //contact encloode
+
+        }
     }
 }
