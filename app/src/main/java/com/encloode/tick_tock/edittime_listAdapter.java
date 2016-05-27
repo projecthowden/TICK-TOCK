7b2f9d96629b0cfe60e079742c36eda54329ea26
Riko Hamblin <r_hamblin@outlook.com>
2016-05-26 17:16:55 -0700

    implemented list adapter class and viewformat for list

---
+++ app/src/main/java/com/encloode/tick_tock/edittime_listAdapter.java
@@ -0,0 +1,43 @@
+package com.encloode.tick_tock;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * Created by Riko Hamblin on 05/26/16.
+ */
+public class edittime_listAdapter extends ArrayAdapter<Employee> implements Serializable {
+
+    public edittime_listAdapter(Context context, ArrayList<Employee> employees) {
+        super(context, 0, employees);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        Employee employee = getItem(position);
+        //Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.edittime_one_listformat, parent, false);
+        }
+        //Lookup view for data population
+        TextView tv_Name = (TextView) convertView.findViewById(R.id.edittime_one_listViewName);
+
+        //populate the data into the template view using the data object
+        tv_Name.setText(employee.getName());
+
+        // tv_id.setText(new DecimalFormat("##").format(employee.getID()).toString());
+        //tv_id.setText(String.format("%1$, 2d", employee.getID()));
+        //return the completed view to render on the screen
+        return convertView;
+    }
+}
+
+
