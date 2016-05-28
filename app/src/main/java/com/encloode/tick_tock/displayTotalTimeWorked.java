568c4eae56e36c7cb9e16037d24d56d4a8b17887
Ibukun <ioluwayo@ryerson.ca>
2016-05-27 21:34:28 -0400

    Completed all the necessary functions to handle the change of layouts within the 2 activities with multiple layouts.

--- app/src/main/java/com/encloode/tick_tock/displayTotalTimeWorked.java
+++ app/src/main/java/com/encloode/tick_tock/displayTotalTimeWorked.java
@@ -1,7 +1,10 @@
 package com.encloode.tick_tock;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ViewUtils;
+import android.view.View;
 
 public class displayTotalTimeWorked extends AppCompatActivity {
     @Override
@@ -10,7 +13,37 @@ public class displayTotalTimeWorked extends AppCompatActivity {
         setContentView(R.layout.displaytotaltime_one);
 
     }
+    //fucntions to handle layout changes in forward progression
+    public void onClickNextTo_2(View view){
+        setContentView(R.layout.displaytotaltime_two);
+    }
+    public void onClickNextTo_3(View view){
+        setContentView(R.layout.displaytotaltime_three);
+    }
+    public void onClickNextTo_4(View view){
+        setContentView(R.layout.displaytotaltime_four);
+    }
+    //functions to handle layout changes in backwards progression
+    public void onClickBackTo_3(View view){
+        setContentView(R.layout.displaytotaltime_three);
+    }
+    public void onClickBackTo_2(View view){
+        setContentView(R.layout.displaytotaltime_two);
+    }
+    public  void onClickBackTo_1(View view){
+        setContentView(R.layout.displaytotaltime_one);
+    }
+    public  void onClickBackTo_menu(View view){
+        Intent intent = new Intent(this,ownermenu.class);
+        startActivity(intent);
+    }
     @Override
     public void onBackPressed() {}
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Global.saveState(this);
+    }
 }
 
